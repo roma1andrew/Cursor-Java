82104933d08b7886360e6ceddca410a2984ac913
Andrew Romanenko <roma1andrew@gmail.com>
2020-06-20 15:19:29 +0300

    Initial commit 1 commit

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import com.cursor.hw.Homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        Homework1 homework1 = new Homework1();
+        homework1.testHomework1();
+    }
+}
---
+++ src/com/cursor/hw/Homework1.java
@@ -0,0 +1,15 @@
+package com.cursor.hw;
+
+public class Homework1 {
+    public void testHomework1(){
+        byte a = 1;
+        short d = 11;
+        long g = 111;
+        float pi = 1.11f;
+        double x = 11.11;
+            System.out.println(a + d);
+            System.out.println(pi / x);
+            System.out.println(d - a);
+            System.out.println(g * a);
+    }
+}
